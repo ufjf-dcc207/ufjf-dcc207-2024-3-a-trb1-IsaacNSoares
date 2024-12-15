7ad2854aa215983fb9c2a5b16ed9ba9661f8c2e9
Isaac Soares <isaacsoares.trabalho@gmail.com>
2024-12-15 17:30:31 -0300

    imprime produtos e filtra por categoria

--- src/components/Categoria/Categoria.jsx
+++ src/components/Categoria/Categoria.jsx
@@ -1,13 +1,18 @@
+import { ReactNode } from "react";
 import "./Categoria.css";
 import Produto from "../Produto/Produto.jsx";
+import PROD from "../../produtos.json";
 
 function Categoria({categ}) {
+
+  const c = PROD.produtos.filter(info => (info.categoria == categ));
+
   return (
     <div className="categoria">
       <h2>{categ}</h2>
-      <Produto/>
-      <Produto/>
-      <Produto/>
+      {c.map(info=>(
+        <Produto key={info.nome} informacoes={info}/>
+      ))}
     </div>
   );
 }

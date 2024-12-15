3ef93918b181cfb17f54789a2e0f86a9a4b32dcf
Isaac Soares <isaacsoares.trabalho@gmail.com>
2024-12-15 11:45:35 -0300

    adiciona esboço em html

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,87 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-
+      <div className="container">
+        <div className="cabecalho">
+          <h1>Lista de Compras</h1>
+        </div>
+        <div className="categoria">
+          <h2>Categoria 1</h2>
+          <div className="produto">
+            <img src="" alt="imagem" />
+            <h3 className="nome">Nome</h3>
+            <p className="descricao">Descrição</p>
+            <p className="loja">Loja</p>
+            <p className="preco">Preço</p>
+          </div>
+          <div className="produto">
+            <img src="" alt="imagem" />
+            <h3 className="nome">Nome</h3>
+            <p className="descricao">Descrição</p>
+            <p className="loja">Loja</p>
+            <p className="preco">Preço</p>
+          </div>
+          <div className="produto">
+            <img src="" alt="imagem" />
+            <h3 className="nome">Nome</h3>
+            <p className="descricao">Descrição</p>
+            <p className="loja">Loja</p>
+            <p className="preco">Preço</p>
+          </div>
+        </div>
+        <div className="categoria">
+          <h2 className="nomeCategoria">Categoria 2</h2>
+          <div className="produto">
+            <img src="" alt="imagem" />
+            <h3 className="nome">Nome</h3>
+            <p className="descricao">Descrição</p>
+            <p className="loja">Loja</p>
+            <p className="preco">Preço</p>
+          </div>
+          <div className="produto">
+            <img src="" alt="imagem" />
+            <h3 className="nome">Nome</h3>
+            <p className="descricao">Descrição</p>
+            <p className="loja">Loja</p>
+            <p className="preco">Preço</p>
+          </div>
+          <div className="produto">
+            <img src="" alt="imagem" />
+            <h3 className="nome">Nome</h3>
+            <p className="descricao">Descrição</p>
+            <p className="loja">Loja</p>
+            <p className="preco">Preço</p>
+          </div>
+        </div>
+        <div className="categoria">
+          <h2 className="nomeCategoria">Categoria 3</h2>
+          <div className="produto">
+            <img src="" alt="imagem" />
+            <h3 className="nome">Nome</h3>
+            <p className="descricao">Descrição</p>
+            <p className="loja">Loja</p>
+            <p className="preco">Preço</p>
+          </div>
+          <div className="produto">
+            <img src="" alt="imagem" />
+            <h3 className="nome">Nome</h3>
+            <p className="descricao">Descrição</p>
+            <p className="loja">Loja</p>
+            <p className="preco">Preço</p>
+          </div>
+          <div className="produto">
+            <img src="" alt="imagem" />
+            <h3 className="nome">Nome</h3>
+            <p className="descricao">Descrição</p>
+            <p className="loja">Loja</p>
+            <p className="preco">Preço</p>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

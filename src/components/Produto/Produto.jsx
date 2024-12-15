8ab989e2129a372c53b12384231389008a7c1362
Isaac Soares <isaacsoares.trabalho@gmail.com>
2024-12-15 14:14:13 -0300

    usa componente no app

---
+++ src/components/Produto/Produto.jsx
@@ -0,0 +1,15 @@
+import "./Produto.css";
+
+function Produto() {
+  return (
+    <div className="produto">
+      <img src="" alt="imagem" />
+      <h3 className="nome">Nome</h3>
+      <p className="descricao">Descrição</p>
+      <p className="loja">Loja</p>
+      <p className="preco">Preço</p>
+    </div>
+  );
+}
+
+export default Produto;

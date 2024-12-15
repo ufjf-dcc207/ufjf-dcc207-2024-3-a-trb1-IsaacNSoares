5e7fce274e8580d56eb7c20580ec03c9ad3e44f2
Isaac Soares <isaacsoares.trabalho@gmail.com>
2024-12-15 19:53:16 -0300

    limpa css da página e inicia css de produto

--- src/components/Produto/Produto.jsx
+++ src/components/Produto/Produto.jsx
@@ -8,9 +8,11 @@ function Produto({informacoes}) {
 
   return (
     <div className="produto">
-      <img src={informacoes.imagem} alt={informacoes.nome} />
+      <img className="imagem" src={informacoes.imagem} alt={informacoes.nome} />
       <h3 className="nome">{informacoes.nome}</h3>
+      <hr />
       <p className="descricao">{informacoes.descricao}</p>
+      <hr />
       <p className="loja">{informacoes.loja}</p>
       <p className="preco">R${valorcorreto}</p>
     </div>
